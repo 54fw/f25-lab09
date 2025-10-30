e306ff1362bf2798710ffe183c3f15e4fea48677
Kangshuo Li <kangshul@andrew.cmu.edu>
2025-10-30 15:07:42 -0400

    implement mainAsync

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,6 @@ const client = new ImageAnnotatorClient();
 function detectFace(fileName: string) {
     console.log(`Running logo detection on ${fileName}`);
     client.logoDetection(fileName)
-    .then(([result]) => {
-        let scores: number[] = [];
-        const logos = result.logoAnnotations;
-        logos?.forEach((logo) => {
-            if (logo.description)
-                console.log(`"${logo.description}" found in in file ${fileName}`);
-            if (logo.score)
-                scores.push(logo.score);
-        });
-        const avg = scores.reduce((a, b) => a + b) / scores.length;
-        console.log(`Average score for ${fileName}: ${avg}`);
-    })
-    .catch((err) => {
-        if (err.code == 'ENOENT')
-            console.error(`File ${fileName} not found`);
-    });
-}
-
-/**
- * Runs logo detection on the given list of file names and logs the description and average score of each logo.
- * @param fileNames - An array of file names to run logo detection on.
- * @returns void
- */
-function main (fileNames: string[]): void {
-    fileNames.forEach((fileName: string) => {
-        console.log(`Running logo detection on ${fileName}`);
-        client.logoDetection(fileName)
         .then(([result]) => {
             let scores: number[] = [];
             const logos = result.logoAnnotations;
@@ -45,24 +18,71 @@ function main (fileNames: string[]): void {
             console.log(`Average score for ${fileName}: ${avg}`);
         })
         .catch((err) => {
-            if (err.code === 'ENOENT')
+            if (err.code == 'ENOENT')
                 console.error(`File ${fileName} not found`);
-            else if (err.code == 7)
-                console.error(err.details);
         });
+}
+
+/**
+ * Runs logo detection on the given list of file names and logs the description and average score of each logo.
+ * @param fileNames - An array of file names to run logo detection on.
+ * @returns void
+ */
+function main(fileNames: string[]): void {
+    fileNames.forEach((fileName: string) => {
+        console.log(`Running logo detection on ${fileName}`);
+        client.logoDetection(fileName)
+            .then(([result]) => {
+                let scores: number[] = [];
+                const logos = result.logoAnnotations;
+                logos?.forEach((logo) => {
+                    if (logo.description)
+                        console.log(`"${logo.description}" found in in file ${fileName}`);
+                    if (logo.score)
+                        scores.push(logo.score);
+                });
+                const avg = scores.reduce((a, b) => a + b) / scores.length;
+                console.log(`Average score for ${fileName}: ${avg}`);
+            })
+            .catch((err) => {
+                if (err.code === 'ENOENT')
+                    console.error(`File ${fileName} not found`);
+                else if (err.code == 7)
+                    console.error(err.details);
+            });
     });
 }
 
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    console.error(new Error("mainAsync not implemented"));
     // Your code here
+    fileNames.forEach(async (fileName: string) => {
+        console.log(`Running logo detection on ${fileName}`);
+        try {
+            const [result] = await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description)
+                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            const avg = scores.reduce((a, b) => a + b) / scores.length;
+            console.log(`Average score for ${fileName}: ${avg}`);
+        } catch (err: any) {
+            if (err.code === 'ENOENT')
+                console.error(`File ${fileName} not found`);
+            else if (err.code == 7)
+                console.error(err.details);
+        }
+    });
 }
 
 main([
-    './images/cmu.jpg', 
-    './images/logo-types-collection.jpg', 
+    './images/cmu.jpg',
+    './images/logo-types-collection.jpg',
     './images/not-a-file.jpg'
 ]);
 
@@ -70,7 +90,7 @@ main([
 await new Promise(r => setTimeout(r, 1000));
 
 mainAsync([
-    './images/cmu.jpg', 
-    './images/logo-types-collection.jpg', 
+    './images/cmu.jpg',
+    './images/logo-types-collection.jpg',
     './images/not-a-file.jpg'
 ]);
